100290716c455764e7382fd6d10b13ed12c334c0
Florian Büchi <54060167+Flopi04LP@users.noreply.github.com>
2021-01-12 09:17:29 +0100

    Update plantWaterer1.0.ino

--- plantWaterer1.0.ino
+++ plantWaterer1.0.ino
@@ -2,12 +2,24 @@
 const int relaisPin = 3;
  
 void setup() {
+  Serial.begin(9600);
+  pinMode(ledRed, OUTPUT);
+  pinMode(ledYellow, OUTPUT);
+  pinMode(ledGreen, OUTPUT);
+  pinMode(relaisPin, OUTPUT);
+  Serial.println("TEST");
   pinMode(relaisPin, OUTPUT);
 }
  
 void loop() {
-  digitalWrite(relaisPin, HIGH);
-  delay(500);
-  digitalWrite(relaisPin, LOW);
-  delay(1000);
+  for (int i = 0; i <= 100; i++) 
+ { 
+   sensorValue = sensorValue + analogRead(SensorPin); 
+   delay(1); 
+ } 
+ sensorValue = sensorValue/100.0; 
+ Serial.println(sensorValue); 
+ delay(30); 
+
+ 
 }
