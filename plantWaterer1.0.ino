d13331330d714149a2936bb92c849bfbd35a1809
Florian Büchi <54060167+Flopi04LP@users.noreply.github.com>
2021-01-12 10:16:38 +0100

    Update plantWaterer1.0.ino

--- plantWaterer1.0.ino
+++ plantWaterer1.0.ino
@@ -2,28 +2,40 @@ const int relaisPin = 3;
 const int ledRed = 11;
 const int ledYellow = 12;
 const int ledGreen = 13;
-#define SensorPin A0 
-float sensorValue = 0; 
+#define MoisturePin A0 
+#define WaterPin A5
+float moistureValue = 0; 
+float waterValue = 0;
 
 
 void setup() {
   Serial.begin(9600);
+  for (int i = 0; i <= 5; i++) 
+ { 
   pinMode(ledRed, OUTPUT);
   pinMode(ledYellow, OUTPUT);
   pinMode(ledGreen, OUTPUT);
   pinMode(relaisPin, OUTPUT);
-  Serial.println("TEST");
+  delay(500);
+  }
+  Serial.println("Waterplanter 3000 started");
   pinMode(relaisPin, OUTPUT);
 }
  
 void loop() {
   for (int i = 0; i <= 100; i++) 
  { 
-   sensorValue = sensorValue + analogRead(SensorPin); 
+   moistureValue = moistureValue + analogRead(MoisturePin); 
+   waterValue = waterValue + analogRead(WaterPin); 
    delay(1); 
  } 
- sensorValue = sensorValue/100.0; 
- Serial.println(sensorValue); 
+ moistureValue = moistureValue/100.0;
+ Serial.println("Moisture:"); 
+ Serial.println(moistureValue); 
+ waterValue = waterValue/100.0; 
+ Serial.println("Water level:"); 
+ Serial.println(waterValue); 
+ 
  delay(30); 
 
  
